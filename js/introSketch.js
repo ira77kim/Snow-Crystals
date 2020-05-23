3f856184bb16e44fa637296f18c88d1a302451cf
ira77kim <53447010+ira77kim@users.noreply.github.com>
2020-05-23 00:32:42 -0400

    edit

---
+++ js/introSketch.js
@@ -0,0 +1,78 @@
+let symmetry =6;
+let angle = 360 / symmetry;
+let saveButton;
+let clearButton;
+let num=0; 
+
+
+document.addEventListener("click",cleartitle);
+
+function cleartitle(){
+  document.getElementById("title").style.display="none";
+
+}
+
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+
+  angleMode(DEGREES);
+  background(59, 126, 172);
+  // saveButton = createButton("save");
+  // saveButton.mousePressed(saveSnowFlake);
+  // clearButton = createButton("clear");
+  // clearButton.mousePressed(clearCanvas);
+//     translate(width/2,height/2);
+//     stroke(255,0,0);
+    
+//     for(let i = 0; i < symmetry; i ++){
+//         rotate(angle);
+//         line(0,0,width,0);
+//       }
+  
+}
+function saveSnowFlake() {
+  save("snowflake"+num);
+  num++;
+}
+
+function clearCanvas() {
+  background(0);
+}
+
+function draw() {  
+  
+
+    translate(width/2,height/2);
+    
+    let mx = mouseX - width/2;
+    let my = mouseY - height/2;
+    let pmx = pmouseX - width/2;
+    let pmy = pmouseY - height/2;
+
+    if(mouseIsPressed){
+      stroke(200,230,255,100);
+      let angle = 360 / symmetry;
+      for(let i = 0; i < symmetry; i++){
+        
+        let distance = dist(mx,my,pmx,pmy);
+        let sw = map(distance, 0, 8, 8, 1);
+        
+        strokeWeight(sw);
+        rotate(angle);
+        
+        push();
+          line(mx,my,pmx,pmy);
+        pop();
+        push();
+          scale(1,-1);
+          line(mx,my,pmx,pmy);
+        pop();
+      }
+      
+    }
+    if (keyIsPressed){
+        if(key ==  " "){
+            background(0);
+        }
+    }
+}
